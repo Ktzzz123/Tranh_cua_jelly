d0563a28b5f7a03b1d6370ddad6e2aa114459806
TieuThiKieuTien/18521491 <18521491@gm.uit.edu.vn>
2021-12-08 01:27:07 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,35 @@
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const fileUpload = require('express-fileupload')
+const cookieParser = require('cookie-parser')
+
+
+const app = express()
+app.use(express.json ())
+app.use(cookieParser())
+app.use(cors())
+app.use(fileUpload({
+    useTempFiles:true
+}))
+
+//connect to mongodb
+const URL = process.env.MONGODB_URL;
+mongoose.connect(URL, {
+    useCreateIndex:true,
+    useFindAndModify:true,
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+},err=>{
+    if(err) throw err;
+    console.log('Connect to database successfully!')
+})
+
+
+
+const PORT =  process.env.PORT || 5000
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`)
+})
