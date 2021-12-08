00f6613414f26b9618c14374f8a114962c2e07a9
TieuThiKieuTien/18521491 <18521491@gm.uit.edu.vn>
2021-12-08 09:07:55 +0700

    add delete and update category api

--- controller/categoryCtrl.js
+++ controller/categoryCtrl.js
@@ -23,6 +23,25 @@ const categoryCtrl ={
          }catch(err){
             return res.status(500).json({smg: err.message})
          }
+    },
+    deleteCategory: async(req, res)=>{
+        try{
+            await Category.findByIdAndDelete(req.params.id)
+            res.json({msg: "deleted successfully!"})
+        }catch(err){
+            return res.status(500).json({smg: err.message})
+
+        }
+    }, 
+    updateCategory: async(req, res)=>{
+        try{
+           const {name}= req.body;
+           await Category.findOneAndUpdate({_id: req.params.id},{name})
+           res.json({msg: 'update successfully!!'})
+        }catch(err){
+            return res.status(500).json({smg: err.message})
+
+        }
     }
 }
 
